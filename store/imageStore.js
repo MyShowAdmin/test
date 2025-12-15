86b1fc3dd91baa8e156f95d860ef28d08b816305
Antoine Bocquet <antoinebocquet@MacBook-Pro-de-Antoine.local>
2025-12-15 15:27:37 +0100

    Ajout du système de hash et BDD

--- store/imageStore.js
+++ store/imageStore.js
@@ -1,12 +1,17 @@
 import { pool } from "../db.js";
 
 export async function savePendingImage({ hash, imageUrl }) {
-  await pool.query(
-    `
-    INSERT INTO images (hash, cloudinary_url, status)
-    VALUES ($1, $2, 'pending')
-    ON CONFLICT (hash) DO NOTHING
-    `,
-    [hash, imageUrl]
-  );
-}
+    console.log('💾 savePendingImage called', { hash, imageUrl });
+  
+    const result = await pool.query(
+      `
+      INSERT INTO images (hash, cloudinary_url, status)
+      VALUES ($1, $2, 'pending')
+      ON CONFLICT (hash) DO NOTHING
+      RETURNING *
+      `,
+      [hash, imageUrl]
+    );
+  
+    console.log('📦 DB result:', result.rows);
+  }
