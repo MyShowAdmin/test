3071cac7202df639d26f3b5e5e1d01e2d07b7af9
Antoine Bocquet <antoinebocquet@MacBook-Pro-de-Antoine.local>
2025-12-16 07:07:49 +0100

    Supression du système de HASH et mise en place de l'ID unique

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import crypto from 'crypto';
 import { addLinksToOrderNote } from "./shopify/updateOrderNote.js";
+import cloudinary from 'cloudinary';
 import { pool } from '../db.js';
 
 const router = express.Router();
